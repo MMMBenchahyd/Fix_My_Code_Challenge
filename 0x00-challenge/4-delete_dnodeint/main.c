366bf3d5d56b8b05b512a48a3c447699fc1023ab
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-01-27 00:53:09 +0100

    up

--- 0x00-challenge/4-delete_dnodeint/main.c
+++ 0x00-challenge/4-delete_dnodeint/main.c
@@ -4,13 +4,14 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX students.
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
 	dlistint_t *head;
+
 	head = NULL;
 	add_dnodeint_end(&head, 0);
 	add_dnodeint_end(&head, 1);
